8617e43b94e683a57e199602f2194ba66eef03a0
Azriel Akbar Firman Syah <Azrielakbar380@gmail.com>
2025-08-05 14:26:44 +0700

    Ok tp belum fix3

--- resources/views/layouts/partials/sidenav.blade.php
+++ resources/views/layouts/partials/sidenav.blade.php
@@ -209,6 +209,23 @@
                 </li>
             </ul>
         </div>
+
+<div id="dropdown-cta" class="p-4 mt-6 rounded-lg bg-green-50 dark:bg-green-900" role="alert">
+  <div class="flex items-center mb-3">
+    <span class="bg-orange-100 text-orange-800 text-sm font-semibold me-2 px-2.5 py-0.5 rounded-sm dark:bg-orange-200 dark:text-orange-900">Development</span>
+    
+    <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-green-50 inline-flex justify-center items-center w-6 h-6 text-green-900 rounded-lg focus:ring-2 focus:ring-green-400 p-1 hover:bg-green-200 dark:bg-green-900 dark:text-green-400 dark:hover:bg-green-800" data-dismiss-target="#dropdown-cta" aria-label="Close">
+      <span class="sr-only">Close</span>
+      <svg class="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+      </svg>
+    </button>
+  </div>
+
+  <p class="text-sm font-medium text-green-800 dark:text-green-300">
+    This Web App Version 1.0.0
+  </p>
+</div>
         
         {{-- Sidenav Toggler - positioned at bottom --}}
         <div class="flex justify-center items-center p-4 border-t border-gray-200 dark:border-gray-700">
