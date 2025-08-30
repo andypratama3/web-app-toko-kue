f5ac7ea33cb82fdb0ddb9b353f6181747d198da6
sandy <sandyasmoro13@gmail.com>
2025-08-30 13:32:50 +0700

    FIX : toast untuk info

--- resources/views/dashboard/kurir/pesanan/create.blade.php
+++ resources/views/dashboard/kurir/pesanan/create.blade.php
@@ -213,6 +213,12 @@
                 iconTextColor = 'text-green-500 dark:text-green-200';
                 iconSvg =
                     `<svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" /></svg>`;
+            } else if (type === 'info') {
+                // Menambahkan kondisi untuk tipe 'info' dengan ikon dan warna biru
+                iconBgColor = 'bg-blue-100 dark:bg-blue-800';
+                iconTextColor = 'text-blue-500 dark:text-blue-200';
+                iconSvg =
+                    `<svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/></svg>`;
             } else { // 'error'
                 iconBgColor = 'bg-red-100 dark:bg-red-800';
                 iconTextColor = 'text-red-500 dark:text-red-200';
