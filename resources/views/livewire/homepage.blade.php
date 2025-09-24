a2bafff3d7090e59d4763dc6a2f64f59688541fa
sandy <sandyasmoro13@gmail.com>
2025-09-24 10:06:08 +0700

    FIX : homepage

--- resources/views/livewire/homepage.blade.php
+++ resources/views/livewire/homepage.blade.php
@@ -66,7 +66,7 @@
   </noscript>
 
   <!-- End Google Tag Manager (noscript) -->
-  <!-- Floating WhatsApp Button 
+  <!-- Floating WhatsApp Button
   <a class="fixed bottom-6 right-6 z-50"
     href="https://wa.me/6282144834303?text=Hai%20admin%20*Pandan%20Asli%20Malang*%2C%20perkenalan%20nama%20saya%20(isi%20nama%20anda)%20.%20Saya%20ingin%20.."
     target="_blank" aria-label="Chat via WhatsApp" style="
@@ -81,7 +81,7 @@
     justify-content: center;
     transition: 0.3s;
   "> -->
-  <!-- WhatsApp 
+  <!-- WhatsApp
     <svg width="26" height="26" viewBox="0 0 26 26" fill="white" xmlns="http://www.w3.org/2000/svg">
       <path fill-rule="evenodd" clip-rule="evenodd"
         d="M18.8663 15.5805C18.5434 15.4191 16.9607 14.6413 16.666 14.533C16.3713 14.4257 16.1568 14.3726 15.9413 14.6955C15.7268 15.0161 15.1103 15.742 14.9229 15.9565C14.7344 16.172 14.547 16.198 14.2253 16.0377C13.9035 15.8752 12.8657 15.5361 11.6361 14.4398C10.6795 13.5861 10.0328 12.532 9.84533 12.2092C9.65792 11.8875 9.82475 11.713 9.98617 11.5527C10.1313 11.4086 10.3079 11.1768 10.4693 10.9894C10.6307 10.8009 10.6838 10.6665 10.7911 10.451C10.8994 10.2365 10.8453 10.049 10.764 9.88763C10.6838 9.72621 10.0403 8.14129 9.77167 7.49671C9.51058 6.86946 9.24517 6.95504 9.048 6.94421C8.8595 6.93554 8.645 6.93338 8.4305 6.93338C8.216 6.93338 7.86717 7.01354 7.5725 7.33638C7.27675 7.65813 6.44583 8.43705 6.44583 10.022C6.44583 11.6058 7.5985 13.1365 7.75992 13.3521C7.92133 13.5666 10.0295 16.8188 13.2589 18.213C14.0281 18.5445 14.6272 18.7428 15.0941 18.8901C15.8654 19.136 16.5674 19.1014 17.1221 19.018C17.7396 18.9259 19.0266 18.239 19.2953 17.4872C19.5628 16.7354 19.5628 16.0908 19.4827 15.9565C19.4025 15.8221 19.188 15.742 18.8652 15.5805H18.8663ZM12.9924 23.6005H12.9881C11.07 23.6008 9.18707 23.0852 7.53675 22.1076L7.14675 21.8758L3.09292 22.9396L4.17517 18.9876L3.92058 18.5825C2.84823 16.8755 2.28074 14.9 2.28367 12.8841C2.28583 6.97996 7.08933 2.17646 12.9968 2.17646C15.8568 2.17646 18.5456 3.29229 20.5671 5.31596C21.5642 6.30892 22.3544 7.48974 22.8922 8.7901C23.43 10.0905 23.7046 11.4845 23.7001 12.8917C23.6979 18.7959 18.8944 23.6005 12.9924 23.6005ZM22.1054 3.77871C20.9118 2.57721 19.4916 1.62454 17.9271 0.975912C16.3626 0.32728 14.6849 -0.00441593 12.9913 4.43923e-05C5.89117 4.43923e-05 0.1105 5.77963 0.108333 12.883C0.105043 15.1437 0.698072 17.3652 1.82758 19.3235L0 26L6.82933 24.2082C8.71853 25.2375 10.8356 25.7768 12.987 25.7769H12.9924C20.0926 25.7769 25.8733 19.9973 25.8754 12.8928C25.8807 11.1998 25.5502 9.52265 24.903 7.95825C24.2559 6.39384 23.3051 4.97327 22.1054 3.77871Z"
@@ -245,7 +245,7 @@
           <div x-show="tab === 'tentang'" x-transition:enter="transition ease-out duration-800" x-transition:enter-start="opacity-0 translate-y-4" x-transition:enter-end="opacity-100 translate-y-0" x-transition:leave="transition ease-in duration-400" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0">
             <div x-data="{ open: false }">
               <p class="text-gray-600 leading-relaxed mb-8">
-                <span x-show="!open">
+                <span x-show="!open" class="text-justify">
                   Kue Pandan Asli, kami adalah perusahaan kuliner yang berfokus pada produksi dan pengembangan kue tradisional berbahan alami tanpa campuran pengawet dan pewarna. Kami berfokus pada bahan bahan alami mulai dari pewarna kami menggunakan 100% pandan pada seluruh produk kami. Kami berkomitmen menghadirkan kue tradisional dengan bahan baku premium, alami dan kekinian.<br>
                   <button @click="open = true" class="inline-flex items-center gap-2 bg-[#8BA870] text-white px-4 py-1.5 rounded-full font-semibold shadow hover:bg-[#7a965e] transition mt-8 group">
                     <svg class="w-4 h-4 text-white group-hover:translate-x-1 transition-transform" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
@@ -301,9 +301,9 @@
   <!-- Products Section -->
   <section id="produk-kami" class="w-full bg-white pt-4 pb-8">
     <!-- [DIUBAH DI SINI] Menambahkan x-init untuk AOS dan x-effect untuk me-refresh animasi -->
-    <div class="w-[90%] max-w-7xl mx-auto" 
-         x-data="{ kategori: 'semua' }" 
-         x-init="AOS.init({ once: true, duration: 800 })" 
+    <div class="w-[90%] max-w-7xl mx-auto"
+         x-data="{ kategori: 'semua' }"
+         x-init="AOS.init({ once: true, duration: 800 })"
          x-effect="$nextTick(() => { AOS.refreshHard() })">
       <!-- Section Header -->
       <div class="text-center mb-6">
@@ -346,8 +346,8 @@
         <div
           class="bg-white rounded-2xl shadow-lg hover:shadow-xl transition-all duration-300 hover:-translate-y-1 relative"
           data-aos="zoom-in"
-          x-data="{ 
-          open: false, 
+          x-data="{
+          open: false,
           prices: [
             { label: 'Isi 3 Kemasan Mika', value: 9000 },
             { label: 'Isi 5 Kemasan Mika', value: 15000 },
@@ -435,8 +435,8 @@
         <div
           class="bg-white rounded-2xl shadow-lg hover:shadow-xl transition-all duration-300 hover:-translate-y-1 relative"
           data-aos="zoom-in"
-          x-data="{ 
-          open: false, 
+          x-data="{
+          open: false,
           prices: [
             { label: 'Isi 10 Kemasan Mika', value: 10000 },
             { label: 'Isi 30 Kemasan Thinwall', value: 35000 }
@@ -496,8 +496,8 @@
         <div
           class="bg-white rounded-2xl shadow-lg hover:shadow-xl transition-all duration-300 hover:-translate-y-1 relative"
           data-aos="zoom-in"
-          x-data="{ 
-          open: false, 
+          x-data="{
+          open: false,
           prices: [
             { label: 'Isi 5 Kemasan Mika', value: 17500 },
             { label: 'Isi 10 Kemasan Thinwall', value: 35500 }
