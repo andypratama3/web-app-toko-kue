b5714d0289e17cc9acc97c42f27c9e26c80641cb
alungggggg <aldinoalungputraanugraha@gmail.com>
2026-05-04 13:23:44 +0700

    policy-privacy

--- routes/web.php
+++ routes/web.php
@@ -48,6 +48,10 @@ Route::get('/', function (Request $request) {
     return view('livewire.homepage');
 });
 
+Route::get("privacy-policy" , function() {
+    return view('policyPrivacy');
+});
+
 
 // Rute Logout manual
 Route::post('/logout', function (Request $request) {
